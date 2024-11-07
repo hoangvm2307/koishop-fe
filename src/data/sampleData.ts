3e5e228455ed1d4cc15f1d15f4e09acf1c57528d
MSI\huyli <huylinhit@gmail.com>
2024-11-07 21:46:42 +0700

    Update Sample Data

---
+++ src/data/sampleData.ts
@@ -0,0 +1,39 @@
+export const SAMPLE_FAV_CUSTOMER_DATA = [
+    { totalOrders: 15, userId: 1 },
+    { totalOrders: 20, userId: 2 },
+    { totalOrders: 10, userId: 3 },
+    { totalOrders: 25, userId: 4 },
+    { totalOrders: 5, userId: 5 }
+  ];
+  
+  export const SAMPLE_RANKING_ORIGIN_DATA = [
+    { totalFish: 6, originName: "Japan" },
+    { totalFish: 12, originName: "USA" },
+    { totalFish: 8, originName: "China" },
+    { totalFish: 15, originName: "Thailand" },
+    { totalFish: 10, originName: "Vietnam" },
+    { totalFish: 20, originName: "Norway" },
+    { totalFish: 5, originName: "India" },
+    { totalFish: 18, originName: "Indonesia" },
+    { totalFish: 14, originName: "Brazil" },
+    { totalFish: 9, originName: "Canada" }
+  ];
+  
+  export const SAMPLE_CONSIGNMENT_BY_TYPE_DATA = {
+    totalConsignmentOnline: 10,
+    totalConsignmentOffline: 5
+  };
+  
+  export const SAMPLE_CONSIGNMENT_BY_MONTH_DATA = [
+    { date: "2024-01-01T00:00:00", totalConsignment: 3 },
+    { date: "2024-02-01T00:00:00", totalConsignment: 5 },
+    { date: "2024-03-01T00:00:00", totalConsignment: 7 },
+    { date: "2024-04-01T00:00:00", totalConsignment: 2 },
+    { date: "2024-05-01T00:00:00", totalConsignment: 4 },
+    { date: "2024-06-01T00:00:00", totalConsignment: 6 },
+    { date: "2024-07-01T00:00:00", totalConsignment: 8 },
+    { date: "2024-08-01T00:00:00", totalConsignment: 10 },
+    { date: "2024-09-01T00:00:00", totalConsignment: 12 },
+    { date: "2024-10-01T00:00:00", totalConsignment: 15 },
+    { date: "2024-11-01T00:00:00", totalConsignment: 2 }
+  ];
